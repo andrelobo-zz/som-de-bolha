198d45760da211fb20c7278330c60627be150d53
andrelobo <loboandre@hotmail.com>
2020-02-06 20:44:06 -0400

    músicas novas II4!

--- src/list.js
+++ src/list.js
@@ -3,7 +3,7 @@ const ListData = [
     id: 1,
     title: "Tu la tienes , yo también",
     url:
-      "https://drive.google.com/file/d/0B0C2ucVFvc3bUlpESUFQVlppN28/view?usp=sharing"
+      "https://thumbs-prod.si-cdn.com/Vj7Cmc62xkQLwQZLiX1SbOV89ik=/420x240/https://public-media.si-cdn.com/filer/cd/0e/cd0efbec-bc15-4f38-894a-7e0e6f5968b8/campfire_edit.jpg"
   },
   {
     id: 2,
